cc220330874e6eeec8805dc99d0d61102abe75f9
kiarash <kiarashazp78@gmail.com>
2025-07-08 00:17:26 +0330

    write cleaning_data and test that

--- datacleaner.py
+++ datacleaner.py
@@ -12,6 +12,12 @@ def read_data_from_path(input_path):
 
 
 def cleaning_data(data):
+    data = data.dropna(subset=['passenger_count'])
+    data = data[0 <= data['fare_amount']]
+    data = data[0 <= data['trip_distance']]
+    data['payment_type'] = data["payment_type"].apply(lambda x: str(x).lower())
+    data['pickup_datetime'] = pd.to_datetime(data['pickup_datetime'], format="%Y-%m-%d %H:%M:%S")
+    data['dropoff_datetime'] = pd.to_datetime(data['dropoff_datetime'], format="%Y-%m-%d %H:%M:%S")
     return data
 
 
@@ -25,3 +31,14 @@ def main():
     cleaned_data = cleaning_data(data)
     final_data = summarize_data(cleaned_data)
     print(final_data)
+
+
+# in_path = "nyc_taxi_sample_100.json"
+# datas = read_data_from_path(input_path=in_path)
+# clean = cleaning_data(datas)
+# dff = clean.iterrows()
+# for index, row in dff:
+#     print(row['pickup_datetime'])
+#     print(type(row['pickup_datetime']))
+#
+
